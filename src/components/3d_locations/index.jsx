2ebbdbbb72cecbd65ede11ad06fcbdfa91f52fea
Victor Pudeyev <piousbox@gmail.com>
2022-08-13 19:36:37 -0500

    ThreePanelDefault (#12)
    Co-authored-by: mac_a2141 <victor@piousbox.com>

--- src/components/3d_locations/index.jsx
+++ src/components/3d_locations/index.jsx
@@ -8,6 +8,7 @@ export { default as Equirectangular2 } from "./Equirectangular2"
 export { default as Equirectangular4 } from "./Equirectangular4"
 
 export { default as TabiversePlanet } from './TabiversePlanet'
+export { default as ThreePanelDefault } from "./ThreePanelDefault"
 export { default as ThreePanelV1 } from "./ThreePanelV1"
 export { default as ThreePanelV2 } from "./ThreePanelV2"
 export { default as ThreePanelV3 } from "./ThreePanelV3"
