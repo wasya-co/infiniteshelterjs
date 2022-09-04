6b8fc25fbfaeea5621e841ad7c43abf04e0b366d
mac_a2141 <victor@piousbox.com>
2022-09-04 16:53:09 -0500

    bugfix on change of config obj structure

--- config/environments/default.js
+++ config/environments/default.js
@@ -10,6 +10,7 @@ module.exports = {
 
   // This router isn't used. _vp_ 2022-09-04
   router: {
+    locationPath: (a) => `/en/locations/show/${a}`, // @TODO: remove this ugliness. _vp_ 2022-09-04
     longTermTokenPath: '/api/users/long_term_token',
     loginPath: '/api/users/login',
   },
