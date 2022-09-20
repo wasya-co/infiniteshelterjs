42d8d74f288ab5833fdeb8ec52cbf8a6e23a8a78
mac_a2141 <victor@piousbox.com>
2022-09-20 13:48:59 -0500

    cleanup
    little

--- config/environments/default.js
+++ config/environments/default.js
@@ -2,11 +2,11 @@
 module.exports = {
   appIndexPath: "src/index", // used in webpack
   apiOrigin: "",
-  domain: "", // required! 2021-08-31
+  domain: "tgm.piousbox.com", // required! 2021-08-31
   debug: false,
-  // homePath: "",
+  // homePath: "<some-path>",
   requireLogin: true,
-  siteTitle: 'Infinite Shelter Location Discovery Guide',
+  siteTitle: 'Infinite Shelter - Location Discovery Guide',
   stripePublicKey: '',
 }
 
