61acbce06180c5b2d8946022e988c594a9352030
mac_a2141 <victor@piousbox.com>
2023-01-22 17:01:10 -0600

    only added _next, its failing to compile still

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,45 +1,72 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
 (() => {
 var exports = {};
-exports.id = 888;
-exports.ids = [888];
+exports.id = "pages/_app";
+exports.ids = ["pages/_app"];
 exports.modules = {
 
-/***/ 962551:
+/***/ "./pages/_app.js":
+/*!***********************!*\
+  !*** ./pages/_app.js ***!
+  \***********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
-/* harmony export */ });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(616689);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var react_toastify_dist_ReactToastify_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(588819);
-/* harmony import */ var react_toastify_dist_ReactToastify_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_toastify_dist_ReactToastify_css__WEBPACK_IMPORTED_MODULE_1__);
-
-
-
-
-
-
-function NextApp(_ref) {
-  var Component = _ref.Component,
-      pageProps = _ref.pageProps;
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(Component, pageProps);
-}
-
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (NextApp);
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"react\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_toastify_dist_ReactToastify_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-toastify/dist/ReactToastify.css */ \"./node_modules/react-toastify/dist/ReactToastify.css\");\n/* harmony import */ var react_toastify_dist_ReactToastify_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_toastify_dist_ReactToastify_css__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var ishlibjs_dist_index_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ishlibjs/dist/index.css */ \"../ishlibjs/dist/index.css\");\n/* harmony import */ var ishlibjs_dist_index_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(ishlibjs_dist_index_css__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var $src_variables_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! $src/variables.css */ \"./src/variables.css\");\n/* harmony import */ var $src_variables_css__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n($src_variables_css__WEBPACK_IMPORTED_MODULE_3__);\n/* harmony import */ var $src_application_scss__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! $src/application.scss */ \"./src/application.scss\");\n/* harmony import */ var $src_application_scss__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n($src_application_scss__WEBPACK_IMPORTED_MODULE_4__);\n\n\n\n\n\n\nfunction NextApp(_ref) {\n  var Component = _ref.Component,\n      pageProps = _ref.pageProps;\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(Component, pageProps);\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (NextApp);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9fYXBwLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQ0E7QUFFQTtBQUNBO0FBQ0E7QUFDQTs7QUFHQSxTQUFTQyxPQUFULE9BQTJDO0VBQUEsSUFBeEJDLFNBQXdCLFFBQXhCQSxTQUF3QjtFQUFBLElBQWJDLFNBQWEsUUFBYkEsU0FBYTtFQUN6QyxvQkFBTywyREFBQyxTQUFELEVBQWVBLFNBQWYsQ0FBUDtBQUNEOztBQUVELGlFQUFlRixPQUFmIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vSW5maW5pdGVTaGVsdGVyLy4vcGFnZXMvX2FwcC5qcz9lMGFkIl0sInNvdXJjZXNDb250ZW50IjpbIlxuaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0J1xuXG5pbXBvcnQgJ3JlYWN0LXRvYXN0aWZ5L2Rpc3QvUmVhY3RUb2FzdGlmeS5jc3MnXG5pbXBvcnQgJ2lzaGxpYmpzL2Rpc3QvaW5kZXguY3NzJ1xuaW1wb3J0ICckc3JjL3ZhcmlhYmxlcy5jc3MnXG5pbXBvcnQgJyRzcmMvYXBwbGljYXRpb24uc2NzcydcblxuXG5mdW5jdGlvbiBOZXh0QXBwKHsgQ29tcG9uZW50LCBwYWdlUHJvcHMgfSkge1xuICByZXR1cm4gPENvbXBvbmVudCB7Li4ucGFnZVByb3BzfSAvPlxufVxuXG5leHBvcnQgZGVmYXVsdCBOZXh0QXBwXG4iXSwibmFtZXMiOlsiUmVhY3QiLCJOZXh0QXBwIiwiQ29tcG9uZW50IiwicGFnZVByb3BzIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/_app.js\n");
 
 /***/ }),
 
-/***/ 588819:
+/***/ "./node_modules/react-toastify/dist/ReactToastify.css":
+/*!************************************************************!*\
+  !*** ./node_modules/react-toastify/dist/ReactToastify.css ***!
+  \************************************************************/
 /***/ (() => {
 
 
 
 /***/ }),
 
-/***/ 616689:
+/***/ "./src/application.scss":
+/*!******************************!*\
+  !*** ./src/application.scss ***!
+  \******************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./src/variables.css":
+/*!***************************!*\
+  !*** ./src/variables.css ***!
+  \***************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "../ishlibjs/dist/index.css":
+/*!**********************************!*\
+  !*** ../ishlibjs/dist/index.css ***!
+  \**********************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
 /***/ ((module) => {
 
 "use strict";
@@ -54,7 +81,7 @@ module.exports = require("react");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(962551));
+var __webpack_exports__ = (__webpack_exec__("./pages/_app.js"));
 module.exports = __webpack_exports__;
 
 })();
